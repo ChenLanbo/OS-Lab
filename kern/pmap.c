0f04d1d5a027c26e2b7f01022211ce75bf5ad4d8
Lanbo Chen <chenlanbo1988@gmail.com>
2011-09-23 00:01:17 -0400

    On branch lab3 Your branch is ahead of 'origin/lab3' by 2 commits. Changes to be committed: 	modified:   kern/pmap.c 		finish allocate envs array and check_boot_pgdir() succeeds

--- kern/pmap.c
+++ kern/pmap.c
@@ -207,6 +207,10 @@ i386_vm_init(void)
 	// Make 'envs' point to an array of size 'NENV' of 'struct Env'.
 	// LAB 3: Your code here.
 
+	// Debug info
+	// cprintf("ENV %u\n", sizeof(struct Env));
+	envs  = (struct Env *)boot_alloc(NENV * sizeof(struct Page), PGSIZE);
+
 	//////////////////////////////////////////////////////////////////////
 	// Now that we've allocated the initial kernel data structures, we set
 	// up the list of free physical pages. Once we've done so, all further
@@ -245,6 +249,8 @@ i386_vm_init(void)
 	//    - the new image at UENVS  -- kernel R, user R
 	//    - envs itself -- kernel RW, user NONE
 	// LAB 3: Your code here.
+	n = ROUNDUP(NENV * sizeof(struct Env), PGSIZE);
+	boot_map_segment(pgdir, UENVS, n, PADDR(envs), PTE_U);
 
 	//////////////////////////////////////////////////////////////////////
 	// Use the physical memory that 'bootstack' refers to as the kernel
